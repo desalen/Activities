1a77078954996b2d2fd20a78c6c29fb70e6a3edb
Desalen <desalen@gmail.com>
2023-01-18 22:40:37 +0200

    end of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Header, List } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
-    
-  return (
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return ( 
     <div className="App">
-   
+      <Header as='h2' icon='user' content='Reactivities' />
+        <List>
+          {activities.map((activity: any )=> 
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          )}
+        </List>
     </div>
   );
 }
